7b7ace8007aa8e0291be842ae823c316544438cd
Bra-Gabby-Marshal <sodeygabriel123@gmail.com>
2023-08-23 22:27:09 +0000

    Olive Project

--- README.md
+++ README.md
@@ -64,3 +64,7 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+Link to documentation
+https://blog.coffeeinc.in/building-a-crud-application-with-laravel-10-project-olive-5fd74874ce36
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/favorites', [FavoriteController::class, 'index'])
+    ->name('favorites.index');
+
+Route::get('/favorites/create', [FavoriteController::class, 'create'])
+    ->name('favorites.create');
+
+Route::post('/favorites', [FavoriteController::class, 'store'])
+    ->name('favorites.store');
+
+Route::get('/favorites/{favorite}', [FavoriteController::class, 'show'])
+    ->name('favorites.show');
+
+Route::get('/favorites/{favorite}/edit', [FavoriteController::class, 'edit'])
+    ->name('favorites.edit');
+
+Route::put('/favorites/{favorite}', [FavoriteController::class, 'update'])
+    ->name('favorites.update');
+
+Route::delete('/favorites/{favorite}', [FavoriteController::class, 'destroy'])
+    ->name('favorites.destroy');
